5cb51ed4a3dd4342e38ee8b69405c621d7c08848
walfredcutaran <100104566+walfredcutaran@users.noreply.github.com>
2023-03-25 15:04:52 +0800

    Update Time Conversion.py

--- Time Conversion.py
+++ Time Conversion.py
@@ -44,14 +44,3 @@ def timeConversion(s):
         ans += s[1] + ':'
         ans += s[2]
         return ans
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    s = input()
-
-    result = timeConversion(s)
-
-    fptr.write(result + '\n')
-
-    fptr.close()
